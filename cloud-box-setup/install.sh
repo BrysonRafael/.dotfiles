0190eef06f5bb6f19d45e909327e006284a83757
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-18 15:47:22 -0600

    Use --keep-zshrc flag when install oh-my-zsh

--- cloud-box-setup/install.sh
+++ cloud-box-setup/install.sh
@@ -33,5 +33,4 @@ stow nvim
 stow zsh
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
