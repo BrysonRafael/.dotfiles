c4314625c6efa25c377e00a79eff8348073378c5
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-18 15:07:38 -0600

    Refactor zsh install

--- cloud-box-setup/install.sh
+++ cloud-box-setup/install.sh
@@ -18,18 +18,17 @@ if ! command which zsh &> /dev/null; then
 
   # Install oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  # Remove exising dotfiles
-  rm -rf ~/.gitignore
-  rm -rf ~/.zshrc
-  
-  # Symlink config files to home directory
-  cd ~/.dotfiles
-  stow git
-  stow nvim
-  stow zsh
-
-  # Switch to zsh
-  chsh -s $(which zsh) $USER
 fi
 
+# Remove exising dotfiles
+rm -rf ~/.gitignore
+rm -rf ~/.zshrc
+
+# Symlink config files to home directory
+cd ~/.dotfiles
+stow git
+stow nvim
+stow zsh
+
+# Switch to zsh
+chsh -s $(which zsh) $USER
