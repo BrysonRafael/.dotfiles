cef0e0e59e896c2b7e3b8e27ba5eb3d839a1cc58
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-19 12:23:03 -0600

    Update exlorer keymap

--- nvim/.config/nvim/lua/b/keymaps.lua
+++ nvim/.config/nvim/lua/b/keymaps.lua
@@ -2,4 +2,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
-keymap.set("n", "<leader>e", vim.cmd.Explore)
+
+-- netrw
+keymap.set("n", "<leader>e", vim.cmd.Lexplore)
+vim.g.netrw_winsize = 30
+vim.g.netrw_liststyle = 3
+
