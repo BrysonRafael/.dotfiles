cbd30cd75599b41a6f3ace3143c0c29e708df874
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-19 14:22:36 -0600

    Switch to nvim-tree

--- nvim/.config/nvim/lua/b/keymaps.lua
+++ nvim/.config/nvim/lua/b/keymaps.lua
@@ -1,10 +1,3 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
-local keymap = vim.keymap
-
--- netrw
-keymap.set("n", "<leader>e", vim.cmd.Lexplore)
-vim.g.netrw_winsize = 30
 vim.g.netrw_liststyle = 3
-
