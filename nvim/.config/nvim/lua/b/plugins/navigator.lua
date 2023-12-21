67f37fd3d3132efec0f38551b6f35d39184e2586
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-21 15:31:38 -0600

    Use vim.keymap.set so we can pass in built in lua function

--- nvim/.config/nvim/lua/b/plugins/navigator.lua
+++ nvim/.config/nvim/lua/b/plugins/navigator.lua
@@ -4,12 +4,12 @@ return {
     local navigator = require("Navigator")
     navigator.setup()
 
-    local set_keymap = vim.api.nvim_set_keymap
+    local set_keymap = vim.keymap.set
     local opts = { noremap = true, silent = true }
-    set_keymap("n", "<C-h>", "<CMD>NavigatorLeft<CR>", opts)
-    set_keymap("n", "<C-l>", "<CMD>NavigatorRight<CR>", opts)
-    set_keymap("n", "<C-k>", "<CMD>NavigatorUp<CR>", opts)
-    set_keymap("n", "<C-j>", "<CMD>NavigatorDown<CR>", opts)
-    set_keymap("n", "<C-p>", "<CMD>NavigatorPrevious<CR>", opts)
+    set_keymap("n", "<C-h>", navigator.left, opts)
+    set_keymap("n", "<C-l>", navigator.right, opts)
+    set_keymap("n", "<C-k>", navigator.up, opts)
+    set_keymap("n", "<C-j>", navigator.down, opts)
+    set_keymap("n", "<C-p>", navigator.previous, opts)
   end
 }
