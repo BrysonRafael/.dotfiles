606a768ba4888581807fdfd25c47e672fde59ee5
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-21 09:47:17 -0600

    Configure test runners and fix which key descriptions

--- nvim/.config/nvim/lua/b/plugins/vim-tmux-runner.lua
+++ nvim/.config/nvim/lua/b/plugins/vim-tmux-runner.lua
@@ -1,1 +1,8 @@
-return { "christoomey/vim-tmux-runner" }
+return {
+  "christoomey/vim-tmux-runner",
+  keys = {
+    { "<leader>ra", "<cmd>VtrAttachToPane<cr>", desc = "[R]unner Attach To Pane" },
+    { "<leader>rf", "<cmd>VtrFocusRunner<cr>", desc = "[R]unner Focus" },
+    { "<leader>rl", "<cmd>VtrSendLinesToRunner<cr>", desc = "Send Lines To [R]unner" },
+  }
+}
