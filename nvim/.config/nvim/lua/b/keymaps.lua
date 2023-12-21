5bd67c2dd4f8c87ea69b70d014acba4195ef7dd4
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-21 17:47:22 -0600

    Rename variable

--- nvim/.config/nvim/lua/b/keymaps.lua
+++ nvim/.config/nvim/lua/b/keymaps.lua
@@ -2,22 +2,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.netrw_liststyle = 3
 
-local keymap = vim.api.nvim_set_keymap
-
+local set_keymap = vim.api.nvim_set_keymap
 
 -- Buffer Navigation --
 
 -- Move to the next buffer
-keymap("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true, desc = "[B]uffer Next" })
-keymap("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true, desc = "[B]uffer Next" })
+set_keymap("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true, desc = "[B]uffer Next" })
+set_keymap("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true, desc = "[B]uffer Next" })
 
 -- Move to the previous buffer
-keymap("n", "<leader>bp", ":bprev<CR>", { noremap = true, silent = true, desc = "[B]uffer Previous" })
-keymap("n", "<S-h>", ":bprev<CR>", { noremap = true, silent = true, desc = "[B]uffer Previous" })
+set_keymap("n", "<leader>bp", ":bprev<CR>", { noremap = true, silent = true, desc = "[B]uffer Previous" })
+set_keymap("n", "<S-h>", ":bprev<CR>", { noremap = true, silent = true, desc = "[B]uffer Previous" })
 
 -- Delete the current buffer
-keymap("n", "<leader>bd", ":bd<CR>", { noremap = true, silent = true, desc = "[B]uffer Delete" })
-keymap("n", "<S-d>", ":bd<CR>", { noremap = true, silent = true, desc = "[B]uffer Delete" })
+set_keymap("n", "<leader>bd", ":bd<CR>", { noremap = true, silent = true, desc = "[B]uffer Delete" })
+set_keymap("n", "<S-d>", ":bd<CR>", { noremap = true, silent = true, desc = "[B]uffer Delete" })
 
 -- Delete all buffers
-keymap("n", "<leader>bda", ":%bd<CR>", { noremap = true, silent = true, desc = "[B]uffer Delete All" })
+set_keymap("n", "<leader>bda", ":%bd<CR>", { noremap = true, silent = true, desc = "[B]uffer Delete All" })
