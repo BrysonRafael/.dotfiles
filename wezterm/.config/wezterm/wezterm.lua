04cce2a8d36379146f6e1683df310291c4117fc9
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-22 06:38:30 -0600

    Move wezterm.lua into .config/wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,4 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This table will hold the configuration.
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
@@ -11,14 +8,13 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = "BlulocoDark"
-config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 0,
-  right = 0,
+  left = "0.5cell",
+  right = "0.5cell",
   top = "0.5cell",
   bottom = 0,
 }
--- and finally, return the configuration to wezterm
+
 return config
