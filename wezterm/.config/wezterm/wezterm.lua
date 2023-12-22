8847e3026267af0e479f6c7e60f01ab8ce21f05a
Bryson Rafael <bryson.rafael@planningcenter.com>
2023-12-22 07:15:13 -0600

    Add keymaps for splits and adjust settings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,12 +9,28 @@ end
 
 config.color_scheme = "BlulocoDark"
 config.font_size = 14
+config.line_height = 1.1
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
   left = "0.5cell",
   right = "0.5cell",
   top = "0.5cell",
   bottom = 0,
+}
+
+config.keys = {
+  -- Option + \ for Vertical Split
+  {
+    key = "-",
+    mods = "ALT",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+    -- Option + - for Horizontal Split
+  {
+    key = "\\",
+    mods = "ALT",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
 }
 
 return config
