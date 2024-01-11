df560106fa7f3d82d57150b1be9532a3b579bacc
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-01-11 15:03:07 -0600

    Increase terminal linehight

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ end
 
 config.color_scheme = "BlulocoDark"
 config.font_size = 14
-config.line_height = 1.1
+config.line_height = 1.2
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
   left = "0.5cell",
