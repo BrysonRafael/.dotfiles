09c9fa181f8c19bd65b53515c995fdcde9c907fb
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-01-11 17:07:06 -0600

    Setup vim-test for wezterm

--- nvim/.config/nvim/lua/b/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/b/plugins/vim-test.lua
@@ -1,9 +1,12 @@
 return {
   "vim-test/vim-test",
-  keys = {
-    { "<leader>tf", "<cmd>TestFile -strategy=vtr<cr>", desc = "[T]est File" },
-    { "<leader>tn", "<cmd>TestNearest -strategy=vtr<cr>", desc = "[T]est Nearest" },
-    { "<leader>tl", "<cmd>TestLast -strategy=vtr<cr>", desc = "[T]est Last" },
-    { "<leader>ta", "<cmd>TestSuite -strategy=vtr<cr>", desc = "[T]est All" },
-  },
+  config = function()
+    vim.g["test#strategy"] = "wezterm"
+
+    local set_keymap = vim.api.nvim_set_keymap
+    set_keymap("n", "<leader>tf", "<cmd>TestFile<cr>", { noremap = true, silent = true, desc = "[T]est File" })
+    set_keymap("n", "<leader>tn", "<cmd>TestNearest<cr>", { noremap = true, silent = true, desc= "[T]est Nearest" })
+    set_keymap("n", "<leader>tl", "<cmd>TestLast<cr>", { noremap = true, silent = true, desc = "[T]est Last" })
+    set_keymap("n", "<leader>ta", "<cmd>TestSuite<cr>", { noremap = true, silent = true, desc = "[T]est All" })
+  end,
 }
