c6a1f3d0fec8f628000cf9a9bb72207f346769e4
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-01-20 18:35:14 -0600

    Fix python lspconfig

--- nvim/.config/nvim/lua/b/lsp/python.lua
+++ nvim/.config/nvim/lua/b/lsp/python.lua
@@ -2,7 +2,14 @@
 -- python language server
 --
 
-require("lspconfig").pylsp.set({
+local lspconfig = require("lspconfig")
+
+lspconfig.pylsp.setup({
+  capabilities = require("b.lsp").capabilities(),
+  on_attach = require("b.lsp").on_attach,
+})
+
+lspconfig.pyright.setup({
   capabilities = require("b.lsp").capabilities(),
   on_attach = require("b.lsp").on_attach,
 })
