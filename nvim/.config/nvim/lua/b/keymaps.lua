3000b9f63c783fa131aa7f46e4287f7fa6a2a174
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-01-23 14:53:15 -0600

    Add keymap to equalize pane widths

--- nvim/.config/nvim/lua/b/keymaps.lua
+++ nvim/.config/nvim/lua/b/keymaps.lua
@@ -4,11 +4,14 @@ vim.g.netrw_liststyle = 3
 
 local set_keymap = vim.api.nvim_set_keymap
 
--- Resize windows with option + arrow keys
+-- Resize buffer panes with option + arrow keys
 set_keymap("n", "<S-Right>", ":vertical resize -2<CR>", { noremap = true, silent = true })
 set_keymap("n", "<S-Left>", ":vertical resize +2<CR>", { noremap = true, silent = true })
 set_keymap("n", "<S-Up>", ":resize -2<CR>", { noremap = true, silent = true })
 set_keymap("n", "<S-Down>", ":resize +2<CR>", { noremap = true, silent = true })
+
+-- Make buffer panes same width
+set_keymap("n", "<leader>=", ":wincmd =<CR>", { noremap = true, silent = true, desc = "[=] Equalize buffer pane widths" })
 
 -- Move to the next buffer
 set_keymap("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
