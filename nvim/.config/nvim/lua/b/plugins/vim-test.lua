92fb52de5a3c9e39977475f227c497ab449becd9
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-02-08 12:15:42 -0600

    Refactor which-key config and keymaps

--- nvim/.config/nvim/lua/b/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/b/plugins/vim-test.lua
@@ -4,9 +4,9 @@ return {
     vim.g["test#strategy"] = "wezterm"
 
     local set_keymap = vim.api.nvim_set_keymap
-    set_keymap("n", "<leader>tf", "<cmd>TestFile<cr>", { noremap = true, silent = true, desc = "[T]est File" })
-    set_keymap("n", "<leader>tn", "<cmd>TestNearest<cr>", { noremap = true, silent = true, desc= "[T]est Nearest" })
-    set_keymap("n", "<leader>tl", "<cmd>TestLast<cr>", { noremap = true, silent = true, desc = "[T]est Last" })
-    set_keymap("n", "<leader>ta", "<cmd>TestSuite<cr>", { noremap = true, silent = true, desc = "[T]est All" })
+    set_keymap("n", "<leader>tf", "<cmd>TestFile<cr>", { noremap = true, silent = true, desc = "[f]ile" })
+    set_keymap("n", "<leader>tn", "<cmd>TestNearest<cr>", { noremap = true, silent = true, desc= "[n]earest" })
+    set_keymap("n", "<leader>tl", "<cmd>TestLast<cr>", { noremap = true, silent = true, desc = "[l]ast" })
+    set_keymap("n", "<leader>ta", "<cmd>TestSuite<cr>", { noremap = true, silent = true, desc = "[a]ll" })
   end,
 }
