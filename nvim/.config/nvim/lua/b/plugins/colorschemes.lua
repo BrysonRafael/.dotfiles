b90b24e7bae26b7c2fa1433cee002450f08c8a99
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-02-22 10:00:26 -0600

    Clean up nvim colorschemes

--- nvim/.config/nvim/lua/b/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/b/plugins/colorschemes.lua
@@ -1,24 +1,5 @@
 return {
   {
-    'catppuccin/nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('nordic').load()
-      vim.cmd.colorscheme 'nordic'
-    end,
-  },
-  {
-    'navarasu/onedark.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
     'Mofiqul/dracula.nvim',
     lazy = false,
     priority = 1000,
@@ -29,23 +10,16 @@ return {
     priority = 1000,
   },
   {
-    'zootedb0t/citruszest.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'sekke276/dark_flat.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'alexmozaidze/palenight.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
     'tanvirtin/monokai.nvim',
     lazy = false,
     priority = 1000,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'carbonfox'
+    end,
   },
 }
