bbbeae08ce4dd7a37395a60eaaacc68ae12b1669
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-02-22 14:48:58 -0600

    Remove emmet lsp and require eslint

--- nvim/.config/nvim/lua/b/lsp/emmet.lua
+++
@@ -1,9 +0,0 @@
---
--- emmet language server
--- https://github.com/olrtg/emmet-language-server
---
-
-require('lspconfig').emmet_language_server.setup {
-  capabilities = require('b.lsp').capabilities(),
-  on_attach = require('b.lsp').on_attach,
-}
