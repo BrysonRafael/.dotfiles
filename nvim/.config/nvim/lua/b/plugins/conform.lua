003886e0f5e510a7816a63ac7f0ac661022fc96e
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-03-05 09:05:09 -0600

    Fix conform configuration

--- nvim/.config/nvim/lua/b/plugins/conform.lua
+++ nvim/.config/nvim/lua/b/plugins/conform.lua
@@ -2,8 +2,19 @@ return {
   'stevearc/conform.nvim',
   config = function()
     require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        eruby = { 'rubocop' },
+        ruby = { 'rubocop' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+      },
       format_on_save = {
         timeout_ms = 500,
+        lsp_fallback = true,
       },
     }
   end,
