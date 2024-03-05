def93baf909926b7f86867426559300375176124
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-03-05 08:48:34 -0600

    Allow formatting on all file types

--- nvim/.config/nvim/lua/b/plugins/conform.lua
+++ nvim/.config/nvim/lua/b/plugins/conform.lua
@@ -2,14 +2,6 @@ return {
   'stevearc/conform.nvim',
   config = function()
     require('conform').setup {
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        javascript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescript = { 'prettier' },
-        typescriptreact = { 'prettier' },
-      },
       format_on_save = {
         timeout_ms = 500,
       },
