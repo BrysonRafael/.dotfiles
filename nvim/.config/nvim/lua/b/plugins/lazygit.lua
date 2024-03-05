955bdf6eedefe67beb52c15032a77c805cf3a26e
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-03-05 09:28:42 -0600

    Fix Lazygit config

--- nvim/.config/nvim/lua/b/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/b/plugins/lazygit.lua
@@ -1,20 +1,13 @@
 return {
   'kdheepak/lazygit.nvim',
-  config = function()
-    require('lazy').setup {
-      {
-        'kdheepak/lazygit.nvim',
-        cmd = {
-          'LazyGit',
-          'LazyGitConfig',
-          'LazyGitCurrentFile',
-          'LazyGitFilter',
-          'LazyGitFilterCurrentFile',
-        },
-        dependencies = {
-          'nvim-lua/plenary.nvim',
-        },
-      },
-    }
-  end,
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
 }
