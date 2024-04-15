93ed7143fa6aa2a2a59e7d65aa2cd7bc2e6d8683
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-04-15 12:11:41 -0500

    Move lazygit keymap to plugin config

--- nvim/.config/nvim/lua/b/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/b/plugins/lazygit.lua
@@ -1,13 +1,9 @@
 return {
   'kdheepak/lazygit.nvim',
-  cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
-  },
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
+  config = function()
+    vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true, desc = 'Git [g]ui' })
+  end,
 }
