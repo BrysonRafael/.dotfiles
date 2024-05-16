2e2fc062868a7a7285cdf715cf14e7255cfcfcd9
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-05-16 16:46:08 -0500

    Refactor Copilot Setup
    - Switch to copilot.lua plugin
    - Setup completion to play nicely with copilot suggestions
    - Setup lualine icon for copilot status visibility

--- nvim/.config/nvim/lua/b/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/b/plugins/nvim-cmp.lua
@@ -17,6 +17,23 @@ return {
 
     -- Completion icons
     'onsails/lspkind.nvim',
+
+    -- Copilot
+    {
+      'zbirenbaum/copilot.lua',
+      config = function()
+        require('copilot').setup {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        }
+      end,
+    },
+    {
+      'zbirenbaum/copilot-cmp',
+      config = function()
+        require('copilot_cmp').setup()
+      end,
+    },
   },
   config = function()
     local cmp = require 'cmp'
@@ -25,12 +42,23 @@ return {
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match '^%s*$' == nil
+    end
+
     cmp.setup {
-      formatting = lspkind.cmp_format {
-        mode = 'symbol',
-        maxwidth = 50,
-        ellipsis_chat = '...',
-        show_labelDetails = true,
+      formatting = {
+        format = lspkind.cmp_format {
+          mode = 'symbol',
+          maxwidth = 50,
+          ellipsis_chat = '...',
+          show_labelDetails = true,
+          symbol_map = { Copilot = '' },
+        },
       },
       snippet = {
         expand = function(args)
@@ -50,15 +78,13 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
+        ['<Tab>'] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
           else
             fallback()
           end
-        end, { 'i', 's' }),
+        end),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
@@ -70,9 +96,28 @@ return {
         end, { 'i', 's' }),
       },
       sources = {
+        { name = 'copilot' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'cmp-nvim-lsp-signature-help' },
+      },
+      sorting = {
+        priority_weight = 2,
+        comparators = {
+          require('copilot_cmp.comparators').prioritize,
+
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
       },
     }
   end,
