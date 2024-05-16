875ef698bb93066b36dc9e666b3a75b23df540b7
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-05-16 06:56:21 -0500

    Remove lazygit plugin
    I prefer opening Lazygit in a new Tmux tab instead of running it in a
    floating window inside of Neovim.

--- nvim/.config/nvim/lua/b/plugins/lazygit.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'kdheepak/lazygit.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  config = function()
-    vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true, desc = 'Git [g]ui' })
-  end,
-}
