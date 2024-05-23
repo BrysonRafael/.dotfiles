fb9fa736e580c90d16feab5650dab5d1c3b67c6d
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-05-23 08:24:48 -0500

    Fix Copilot Chat keymaps
    A handful of the Copilot Chat commands require lines to be selected in
    order to work properly. Let's update the keymaps for those commands to
    work in visual mode.

--- nvim/.config/nvim/lua/b/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/b/plugins/copilot-chat.lua
@@ -13,7 +13,7 @@ return {
       local function quick_chat()
         local input = vim.fn.input 'Quick Chat: '
         if input ~= '' then
-          require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+          chat.ask(input, { selection = chat.select.buffer })
         end
       end
 
@@ -25,13 +25,13 @@ return {
       set_keymap('n', '<leader>cR', ':CopilotChatReset<CR>', { noremap = true, silent = true, desc = '[R]eset' })
       set_keymap('n', '<leader>cS', ':CopilotChatSave<CR>', { noremap = true, silent = true, desc = '[S]ave' })
       set_keymap('n', '<leader>cL', ':CopilotChatLoad<CR>', { noremap = true, silent = true, desc = '[L]oad' })
-      set_keymap('n', '<leader>ce', ':CopilotChatExplain<CR>', { noremap = true, silent = true, desc = '[e]xplain selected code' })
-      set_keymap('n', '<leader>cr', ':CopilotChatReview<CR>', { noremap = true, silent = true, desc = '[r]eview selected code' })
-      set_keymap('n', '<leader>cf', ':CopilotChatFix<CR>', { noremap = true, silent = true, desc = '[f]ix selected code' })
       set_keymap('n', '<leader>cF', ':CopilotChatFixDiagnostic<CR>', { noremap = true, silent = true, desc = '[F]ix diagnostic issue in file' })
-      set_keymap('n', '<leader>co', ':CopilotChatOptimize<CR>', { noremap = true, silent = true, desc = '[o]ptimize selected code' })
-      set_keymap('n', '<leader>cd', ':CopilotChatDocs<CR>', { noremap = true, silent = true, desc = '[d]ocument selected code' })
-      set_keymap('n', '<leader>cT', ':CopilotChatTests<CR>', { noremap = true, silent = true, desc = 'write [T]ests for selected code' })
+      set_keymap('x', '<leader>ce', ':CopilotChatExplain<CR>', { noremap = true, silent = true, desc = '[e]xplain selected code' })
+      set_keymap('x', '<leader>cr', ':CopilotChatReview<CR>', { noremap = true, silent = true, desc = '[r]eview selected code' })
+      set_keymap('x', '<leader>cf', ':CopilotChatFix<CR>', { noremap = true, silent = true, desc = '[f]ix selected code' })
+      set_keymap('x', '<leader>co', ':CopilotChatOptimize<CR>', { noremap = true, silent = true, desc = '[o]ptimize selected code' })
+      set_keymap('x', '<leader>cd', ':CopilotChatDocs<CR>', { noremap = true, silent = true, desc = '[d]ocument selected code' })
+      set_keymap('x', '<leader>cT', ':CopilotChatTests<CR>', { noremap = true, silent = true, desc = 'write [T]ests for selected code' })
     end,
   },
 }
