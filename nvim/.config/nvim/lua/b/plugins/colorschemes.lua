b7b53f4f6ac83c2f994145b65ea39b646e1caa54
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-05-24 09:10:21 -0500

    Remove unused colorschemes

--- nvim/.config/nvim/lua/b/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/b/plugins/colorschemes.lua
@@ -1,20 +1,5 @@
 return {
   {
-    'rose-pine/neovim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'embark-theme/vim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'Mofiqul/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
