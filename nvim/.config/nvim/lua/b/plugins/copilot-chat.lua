70482ddded42f0cdf2ecc753ac1b254741ffb203
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-05-28 09:45:49 -0500

    Neovim - Fix keymaps for Copilot Chat
    `mapmode-v` allows for visual and select modes.

--- nvim/.config/nvim/lua/b/plugins/copilot-chat.lua
+++ nvim/.config/nvim/lua/b/plugins/copilot-chat.lua
@@ -26,12 +26,12 @@ return {
       set_keymap('n', '<leader>cS', ':CopilotChatSave<CR>', { noremap = true, silent = true, desc = '[S]ave' })
       set_keymap('n', '<leader>cL', ':CopilotChatLoad<CR>', { noremap = true, silent = true, desc = '[L]oad' })
       set_keymap('n', '<leader>cF', ':CopilotChatFixDiagnostic<CR>', { noremap = true, silent = true, desc = '[F]ix diagnostic issue in file' })
-      set_keymap('x', '<leader>ce', ':CopilotChatExplain<CR>', { noremap = true, silent = true, desc = '[e]xplain selected code' })
-      set_keymap('x', '<leader>cr', ':CopilotChatReview<CR>', { noremap = true, silent = true, desc = '[r]eview selected code' })
-      set_keymap('x', '<leader>cf', ':CopilotChatFix<CR>', { noremap = true, silent = true, desc = '[f]ix selected code' })
-      set_keymap('x', '<leader>co', ':CopilotChatOptimize<CR>', { noremap = true, silent = true, desc = '[o]ptimize selected code' })
-      set_keymap('x', '<leader>cd', ':CopilotChatDocs<CR>', { noremap = true, silent = true, desc = '[d]ocument selected code' })
-      set_keymap('x', '<leader>cT', ':CopilotChatTests<CR>', { noremap = true, silent = true, desc = 'write [T]ests for selected code' })
+      set_keymap('v', '<leader>ce', ':CopilotChatExplain<CR>', { noremap = true, silent = true, desc = '[e]xplain selected code' })
+      set_keymap('v', '<leader>cr', ':CopilotChatReview<CR>', { noremap = true, silent = true, desc = '[r]eview selected code' })
+      set_keymap('v', '<leader>cf', ':CopilotChatFix<CR>', { noremap = true, silent = true, desc = '[f]ix selected code' })
+      set_keymap('v', '<leader>co', ':CopilotChatOptimize<CR>', { noremap = true, silent = true, desc = '[o]ptimize selected code' })
+      set_keymap('v', '<leader>cd', ':CopilotChatDocs<CR>', { noremap = true, silent = true, desc = '[d]ocument selected code' })
+      set_keymap('v', '<leader>cT', ':CopilotChatTests<CR>', { noremap = true, silent = true, desc = 'write [T]ests for selected code' })
     end,
   },
 }
