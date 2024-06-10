0fa3ed7ca4b922b0f44e01dc6cd997a670e148d7
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-06-10 13:08:00 -0500

    nvim - Remove copilot cmp plugin

--- nvim/.config/nvim/lua/b/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/b/plugins/nvim-cmp.lua
@@ -17,23 +17,6 @@ return {
 
     -- Completion icons
     'onsails/lspkind.nvim',
-
-    -- Copilot
-    {
-      'zbirenbaum/copilot.lua',
-      config = function()
-        require('copilot').setup {
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        }
-      end,
-    },
-    {
-      'zbirenbaum/copilot-cmp',
-      config = function()
-        require('copilot_cmp').setup()
-      end,
-    },
   },
   config = function()
     local cmp = require 'cmp'
@@ -42,32 +25,40 @@ return {
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup {}
 
-    local has_words_before = function()
-      if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
-        return false
-      end
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match '^%s*$' == nil
-    end
-
     cmp.setup {
-      formatting = {
-        format = lspkind.cmp_format {
-          mode = 'symbol',
-          maxwidth = 50,
-          ellipsis_chat = '...',
-          show_labelDetails = true,
-          symbol_map = { Copilot = '' },
-        },
-      },
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
+
       completion = {
         completeopt = 'menu,menuone,noinsert',
       },
+
+      formatting = {
+        format = lspkind.cmp_format {
+          mode = 'symbol_text',
+          preset = 'codicons',
+          before = function(entry, vim_item)
+            vim_item.menu = ({
+              buffer = '[Buffer]',
+              dap = '[DAP]',
+              git = '[GitHub]',
+              luasnip = '[Snippet]',
+              nvim_lsp = '[LSP]',
+              nvim_lsp_signature_help = '[Signature]',
+              path = '[Path]',
+              treesitter = '[Treesitter]',
+            })[entry.source.name]
+
+            vim_item.dup = { feature_flipper = 1, nvim_lsp = 0 }
+
+            return vim_item
+          end,
+        },
+      },
+
       mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -78,13 +69,6 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
-        ['<Tab>'] = vim.schedule_wrap(function(fallback)
-          if cmp.visible() and has_words_before() then
-            cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-          else
-            fallback()
-          end
-        end),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
@@ -95,29 +79,13 @@ return {
           end
         end, { 'i', 's' }),
       },
-      sources = {
-        { name = 'copilot' },
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'cmp-nvim-lsp-signature-help' },
-      },
-      sorting = {
-        priority_weight = 2,
-        comparators = {
-          require('copilot_cmp.comparators').prioritize,
 
-          -- Below is the default comparitor list and order for nvim-cmp
-          cmp.config.compare.offset,
-          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-          cmp.config.compare.exact,
-          cmp.config.compare.score,
-          cmp.config.compare.recently_used,
-          cmp.config.compare.locality,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-        },
+      sources = {
+        { name = 'buffer' },
+        { name = 'luasnip' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'treesitter' },
       },
     }
   end,
