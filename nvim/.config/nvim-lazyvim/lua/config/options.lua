48329bfeb9592e2b90e11a47aa519148761153a5
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-08-28 08:51:01 -0500

    nvim - Move custom Ruby lsp logic to plugin config

--- nvim/.config/nvim-lazyvim/lua/config/options.lua
+++ nvim/.config/nvim-lazyvim/lua/config/options.lua
@@ -5,10 +5,3 @@
 -- this file grows indefinitely and I only care about the current session
 os.remove(vim.env.HOME .. "/.local/state/nvim/lsp.log")
 vim.lsp.set_log_level("WARN") -- other options: "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "OFF"
-
-require("lazyvim.util").lsp.on_attach(function(client, _)
-  if client.name == "ruby_lsp" or client.name == "solargraph" then
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-  end
-end)
