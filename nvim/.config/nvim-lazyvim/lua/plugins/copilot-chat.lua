88b9ac3227303becc44ae277e342a6357fe6a552
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-09-06 10:52:28 -0500

    nvim - Refactor copilot-chat.lua
    Remove unecessary table

--- nvim/.config/nvim-lazyvim/lua/plugins/copilot-chat.lua
+++ nvim/.config/nvim-lazyvim/lua/plugins/copilot-chat.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    opts = {
-      window = {
-        layout = "float",
-        border = "rounded",
-        width = 0.75,
-        height = 0.75,
-      },
+  "CopilotC-Nvim/CopilotChat.nvim",
+  opts = {
+    window = {
+      layout = "float",
+      border = "rounded",
+      width = 0.75,
+      height = 0.75,
     },
   },
 }
