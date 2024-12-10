399313a2fb80c8adda4a5f7b5d9eaa460c5871a6
Bryson Rafael <bryson.rafael@planningcenter.com>
2024-12-10 10:54:54 -0600

    nvim - Disable completion in scratch buffer

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Disable relative line numbers in insert mode
 vim.api.nvim_create_autocmd({ "InsertEnter" }, {
   pattern = "*",
   callback = function()
@@ -10,10 +11,33 @@ vim.api.nvim_create_autocmd({ "InsertEnter" }, {
   end,
 })
 
+-- Enable relative line numbers in not in insert mode
 vim.api.nvim_create_autocmd({ "InsertLeave" }, {
   pattern = "*",
   callback = function()
     vim.wo.number = true
     vim.wo.relativenumber = true
+  end,
+})
+
+-- Disable LSP, completion, and Copilot in scratch buffers
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.buftype == "nofile" then
+      -- Stop LSP clients attached to the current buffer
+      local clients = vim.lsp.get_active_clients()
+      for _, client in ipairs(clients) do
+        if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+          vim.lsp.stop_client(client.id)
+        end
+      end
+
+      -- Disable autocomplete (assuming you use nvim-cmp)
+      require("cmp").setup.buffer({ enabled = false })
+
+      -- Disable GitHub Copilot
+      vim.b.copilot_enabled = false
+    end
   end,
 })
