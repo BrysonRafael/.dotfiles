d2be0656c5c5b839dc176ba54687da5cd7066aa1
Bryson Rafael <bryson.rafael@planningcenter.com>
2025-01-16 09:14:31 -0600

    nvim - Fix broken scratch buffer config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -19,22 +19,3 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
     vim.wo.relativenumber = true
   end,
 })
-
--- Disable LSP, completion, and Copilot in scratch buffers
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = function()
-    if vim.bo.buftype == "nofile" then
-      -- Stop LSP clients attached to the current buffer
-      local clients = vim.lsp.get_clients()
-      for _, client in ipairs(clients) do
-        if client.attached_buffers[vim.api.nvim_get_current_buf()] then
-          vim.lsp.stop_client(client.id)
-        end
-      end
-
-      -- Disable GitHub Copilot
-      vim.b.copilot_enabled = false
-    end
-  end,
-})
---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+---@diagnostic disable: undefined-doc-name
+
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    scratch = {
+      ft = "markdown",
+    },
+  },
+}
