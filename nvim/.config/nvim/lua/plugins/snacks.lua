405cfff387b50416da2b345fd7e1cf9af1948925
Bryson Rafael <bryson.rafael@planningcenter.com>
2025-03-20 06:10:09 -0500

    nvim - Use snacks file explorer

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,14 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+        },
+        files = { hidden = true },
+      },
+    },
     scratch = {
       ft = "markdown",
     },
