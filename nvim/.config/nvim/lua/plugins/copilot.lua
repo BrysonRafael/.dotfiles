f824c299e56d6a47d384b5c2701de8897dc8898e
Bryson Rafael <bryson.rafael@planningcenter.com>
2025-04-07 10:46:50 -0500

    nvim - Fully disable copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,4 @@
 return {
   "zbirenbaum/copilot.lua",
-  opts = {
-    suggestion = {
-      enabled = false,
-      auto_trigger = false,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-  },
+  enabled = false,
 }
