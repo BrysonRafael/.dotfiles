a4fed35fd449fed41a3db6218238e37b9cdd388f
Bryson Rafael <bryson.rafael@planningcenter.com>
2025-05-14 08:38:25 -0500

    Kanagawa

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,29 @@
 return {
   {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    priority = 1000,
+    lazy = false,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "kanagawa-wave",
     },
   },
 }
