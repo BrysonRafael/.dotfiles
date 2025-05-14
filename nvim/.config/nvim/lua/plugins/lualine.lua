eb1235af2da49c89ac092eeaeb1ac80c896f991a
Bryson Rafael <bryson.rafael@planningcenter.com>
2025-05-14 09:51:12 -0500

    nvim - Update lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,11 @@ return {
       section_separators = "",
     },
     sections = {
-      lualine_z = {},
+      lualine_b = { "branch" },
+      lualine_c = { { "filename", path = 1 } },
+      lualine_x = { "filetype" },
+      lualine_y = {},
+      lualine_z = { "location" },
     },
   },
 }
