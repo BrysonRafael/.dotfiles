4e41c9e1bb38c1f6e2ea752a93ac274e635a539e
Bryson Rafael <bryson.rafael@planningcenter.com>
2025-07-01 20:10:42 -0500

    Seoulbones

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,14 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "thesimonho/kanagawa-paper.nvim",
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-paper-canvas",
+      colorscheme = "seoulbones",
     },
   },
 }
