bffc2c33439a2aac408b8a12ea1ec8e298e60778
Bryson Rafael <bryson.rafael@planningcenter.com>
2026-03-09 16:50:49 -0500

    nvim - vscode light modern theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,37 @@
 return {
   {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
+    "gmr458/vscode_modern_theme.nvim",
     lazy = false,
     priority = 1000,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-    },
+    config = function()
+      require("vscode_modern").setup({
+        cursorline = true,
+        transparent_background = false,
+        nvim_tree_darker = true,
+      })
+      vim.o.background = "light"
+
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "vscode_modern",
+        callback = function()
+          local hl = vim.api.nvim_set_hl
+          hl(0, "NormalFloat", { bg = "#ffffff" })
+          hl(0, "FloatBorder", { bg = "#ffffff", fg = "#000000" })
+          hl(0, "SnacksPickerList", { bg = "#ffffff" })
+          hl(0, "SnacksPickerListBorder", { bg = "#ffffff", fg = "#000000" })
+          hl(0, "SnacksPickerPreview", { bg = "#ffffff" })
+          hl(0, "SnacksPickerPreviewBorder", { bg = "#ffffff", fg = "#000000" })
+          hl(0, "SnacksPickerInput", { bg = "#ffffff" })
+          hl(0, "SnacksPickerInputBorder", { bg = "#ffffff", fg = "#000000" })
+          hl(0, "SnacksPickerPrompt", { fg = "#000000", bg = "#ffffff" })
+        end,
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "seoulbones",
+      colorscheme = "vscode_modern",
     },
   },
 }
