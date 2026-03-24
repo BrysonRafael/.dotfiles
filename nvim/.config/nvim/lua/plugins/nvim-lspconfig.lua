0340781cf9f148c6593890794b8a3ff90abc2ff4
Bryson Rafael <bryson.rafael@planningcenter.com>
2026-03-24 09:42:31 -0500

    nvim - Fix config

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -7,38 +7,15 @@ return {
 
     opts.servers = vim.tbl_deep_extend("force", opts.servers, {
       bashls = {
-        settings = {
-          filetypes = { "sh", "zsh" },
-        },
+        filetypes = { "sh", "zsh" },
       },
       ruby_lsp = {
         mason = false,
-        cmd_env = {
-          GEM_HOME = vim.env.RUBY_CONFDIR,
-        },
-        init_options = {
-          formatter = "rubocop",
-          linters = { "rubocop" },
-        },
-      },
-      syntax_tree = {
-        mason = false,
-        cmd_env = {
-          GEM_HOME = vim.env.RUBY_CONFDIR,
-        },
+        reuse_client = function(client, config)
+          return client.root_dir == config.root_dir
+        end,
       },
     })
-
-    opts.setup = {
-      on_attach = function(client, bufnr)
-        require("lazyvim.util").lsp.on_attach(client, bufnr)
-
-        if client.name == "ruby_lsp" or client.name == "solargraph" then
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end
-      end,
-    }
 
     return opts
   end,
